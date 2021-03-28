37e98ba832a90603a98dccc48f4bc9cfb16b1eea
Joseph-G9 <joseph.gong9@gmail.com>
2021-03-28 02:30:42 -0400

    Created user.py

---
+++ user.py
@@ -0,0 +1,21 @@
+"""
+User is the database of the all their books.
+
+TODO
+    Username/Password
+    List of books
+    Add
+    Remove
+    toString
+
+
+"""
+class User:
+    def __init__(self):
+        self.books = null
+
+    def __init__(self, list ):
+        self.books = books
+
+    def add(Book : bookAdded):
+        books.add(bookAdded)
