38c3f7feb999b1a4e8ec3edcef4f2fe061837785
EthanL <ethan.bot.360@gmail.com>
2021-03-28 01:16:14 -0400

    Update books.py

--- books.py
+++ books.py
@@ -8,6 +8,7 @@ class Book(object):
         self.author = author
         self.genre = genre
         self.addDate = today.strftime("%B %d, %Y")
+        self.lentTo = "*"
         
     @property
     def title(self):
@@ -35,13 +36,55 @@ class Book(object):
        
     def addDate(self):
         return self.__addDate
+       
+    @property
+    def lentTo(self):
+        return self.__lentTo
+
+    @lentTo.setter
+    def lentTo(self, name):
+       self.__lentTo = name
 
     def __str__(self):
-        return 'Title: {}| Author: {}| Genre: {}|Date added: {}'.format(self.title, self.author, self.genre, self.addDate)
+        return 'Title: {}| Author: {}| Genre: {}|Date added: {}|Is Lent? {}'.format(self.title, self.author, self.genre, self.addDate, self.isLent())
 
-'''    
+    def isLent(self):
+        if(self.lentTo == "*"):
+            return False
+        return True
+
+    def unLend(self):
+        self.__lentTo = "*"
+
+
+'''
 book1 = Book("James and the Giant Peach", "Roald Dahl", "Childrens")
 print(book1.title)
 print(book1.addDate)
 print(book1)
-'''       
+book1.genre = "YA"
+book1.lentTo = "Jeremy"
+print(book1)
+'''
+
+#Sorts
+def titleSort(books):
+    newList = sorted(books, key=lambda book: (book.title, book.author, book.genre))
+    return newList
+
+def authorSort(books):
+    newList = sorted(books, key=lambda book: (book.author, book.title, book.genre))
+    return newList
+
+def genreSort(books):
+    newList = sorted(books, key=lambda book: (book.genre, book.title, book.author))
+    return newList
+
+
+
+
+
+
+
+
+ 
